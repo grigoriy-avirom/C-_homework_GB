72ba5340c77977a189cbfe0fb5a6a500df19a6c5
Grigoriy Avirom <88avirom@gmail.com>
2022-11-17 21:39:25 +0300

    Четвёртая задача done

--- Seminar_1.cs
+++ Seminar_1.cs
@@ -91,4 +91,22 @@ else
 8 -> 2, 4, 6, 8
 */
 
+/*
+Console.WriteLine("Введите положительное число для манипуляций :)");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
 
+if (number <= 0)
+{
+    Console.WriteLine("Введённое число не положительное :(");
+}
+else
+{
+    Console.WriteLine($"Все чётные числа от 0 до {number} :");
+    while (count <= number)
+    {
+        Console.Write($"{count} ");
+        count = count + 2;
+    }
+}
+*/
