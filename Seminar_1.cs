f9fa05a5fc811169ab3bcde2301b1ad84baa5be9
Grigoriy Avirom <88avirom@gmail.com>
2022-11-17 20:44:20 +0300

    Вторая задача done

--- Seminar_1.cs
+++ Seminar_1.cs
@@ -30,17 +30,40 @@ else
 }
 */
 
-
-
-
-
-
 /*
 Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
 */
+
+/*
+Console.WriteLine("Введите три числа по очереди.");
+Console.WriteLine("");
+Console.WriteLine("Вводите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int bigest = 0;
+
+if (numberA > numberB)
+{
+    bigest = numberA;
+}
+if (numberB > bigest)
+{
+    bigest = numberB;
+}
+if (numberC > bigest)
+{
+    bigest = numberC;
+}
+
+Console.WriteLine($"Самое большое число: {bigest} !");
+*/
+
 
 /*
 Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
