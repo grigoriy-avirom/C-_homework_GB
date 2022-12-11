a1319ae1d8a41a2a250c9aaf0404561481cf7951
Grigoriy Avirom <88avirom@gmail.com>
2022-12-11 22:02:36 +0300

    Home work 6 done

--- Seminar_test.cs
+++ Seminar_test.cs
@@ -163,6 +163,8 @@ Console.WriteLine($"Summ of negative elements is {resaltNeg}");
  */
 
 /* 
+
+
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
     int[] newArray = new int[size];
@@ -202,5 +204,129 @@ int size2;
 if (array_size % 2 == 0)
     size2 = array_size / 2;
 else size2 = (array_size + 1) / 2;
+
+ */
+
+// РАЗВОРОТ МАССИВА
+
+/* 
+int[] CreateNewArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowNewArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] ReverseArray(int[] array)
+{
+    int k = array.Length;
+    for (int i = 0; i < k / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[k - 1 - i];
+        array[k - 1 - i] = temp;
+    }
+    return array;
+}
+
+Console.WriteLine("Input size of  array: ");
+int count_of_elements = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input min possible value of elements: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input max possible value of elements: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[] newArray = CreateNewArray(count_of_elements, minValue, maxValue);
+ShowNewArray(newArray);
+
+Console.WriteLine();
+
+int[] reverseArray = ReverseArray(newArray);
+ShowNewArray(reverseArray);
+
+ */
+
+// ФИББОНАЧЧИ
+/* 
+void Fibonachi(int count, int a, int b)
+{
+    int[] fibArray = new int[count];
+    fibArray[0] = a;
+    fibArray[1] = b;
+    for (int i = 2; i < count; i++)
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    
+    ShowArray(fibArray);
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+Console.WriteLine("Input count of elements: ");
+int count_of_elements = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input first element: ");
+int first_element = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second element: ");
+int second_element = Convert.ToInt32(Console.ReadLine());
+
+Fibonachi(count_of_elements, first_element, second_element);
+ */
+
+// ТРЕУГОЛЬНИК, ТРИ ЧИСЛА
+/* 
+bool Triangle(int side1, int side2, int side3)
+{
+    if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2)
+        return true;
+    else return false;
+}
+
+Console.WriteLine("Input first side: ");
+int first_side = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second side: ");
+int second_side = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third side: ");
+int third_side = Convert.ToInt32(Console.ReadLine());
+
+bool result = Triangle(first_side, second_side, third_side);
+if (result == true)
+    Console.WriteLine("Triangle exist!");
+else Console.WriteLine("Triangle not exist!");
+
+ */
+
+// ДЕСЯТИЧНОЕ В ДВОИЧНОЕ
+
+/* 
+string BinarSyst(int num)
+{
+    string binResult = string.Empty;
+    while (num != 0)
+    {
+        binResult = num % 2 + binResult;
+        num /= 2;
+    }
+    return binResult;
+}
+
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string res = BinarSyst(number);
+Console.WriteLine($"Your number {number} in binar syst is {res}");
 
  */
